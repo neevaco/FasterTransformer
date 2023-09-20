ae4b890d3839188be9444d97bb9e61e40c1f127d
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-19 22:33:20 -0700

    commit

--- examples/cpp/bart/bart_triton_example.cc
+++ examples/cpp/bart/bart_triton_example.cc
@@ -229,8 +229,8 @@ prepareRequest(std::string ini_name, const int node_id, const int gpu_count, std
 
     const size_t request_batch_size = reader.GetInteger("request", "request_batch_size");
 
-    const int start_id = reader.GetInteger("llama_7b", "start_id");
-    const int end_id   = reader.GetInteger("llama_7b", "end_id");
+    const int start_id = reader.GetInteger("decoder", "start_id");
+    const int end_id   = reader.GetInteger("decoder", "end_id");
 
     std::vector<int> v_start_ids;
     std::vector<int> v_start_lengths;
