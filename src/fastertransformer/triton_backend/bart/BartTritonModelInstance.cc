c0fc6fa6a5dd9ce47bef7726e867eb90554b31ba
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-20 21:28:39 -0700

    commit

--- src/fastertransformer/triton_backend/bart/BartTritonModelInstance.cc
+++ src/fastertransformer/triton_backend/bart/BartTritonModelInstance.cc
@@ -106,7 +106,7 @@ BartTritonModelInstance<T>::forward(std::shared_ptr<std::unordered_map<std::stri
         std::cout << "Key: " << pair.first << std::endl;
     }
 
-    printf("input_tensors input_ids %d", 2);
+    printf("input_tensors input_ids");
     input_tensors->at("input_lengths");
     const size_t request_batch_size = input_tensors->at("input_ids").shape[0];
     const size_t mem_max_seq_len    = input_tensors->at("input_ids").shape[1];
