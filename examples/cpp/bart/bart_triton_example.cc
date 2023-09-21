017b5f5ddb788998c4410c141856e193451e3c15
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-20 21:39:28 -0700

    commit

--- examples/cpp/bart/bart_triton_example.cc
+++ examples/cpp/bart/bart_triton_example.cc
@@ -383,7 +383,7 @@ int main(int argc, char* argv[])
     const int  batch_size   = output_tensors_lists[0].get()->at("output_ids").shape[0];
     const int  beam_width   = output_tensors_lists[0].get()->at("output_ids").shape[1];
     const int  seq_len      = output_tensors_lists[0].get()->at("output_ids").shape[2];
-    const int* d_input_lengths = (const int*)output_tensors_lists[0].get()->at("input_lengths").data;
+    const int* d_input_lengths = (const int*)output_tensors_lists[0].get()->at("sequence_length").data;
     // step 6: check results
     if (node_id == 0) {
 
