6841c8b83530d9082e006c7df22d7e0a4e4c151d
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-21 22:03:00 -0700

    commit

--- src/fastertransformer/models/bart/BartDecoder.cc
+++ src/fastertransformer/models/bart/BartDecoder.cc
@@ -552,7 +552,7 @@ void BartDecoder<T>::forward(std::vector<Tensor>*                           outp
                     int st = local_batch_size * d_model_;
                     buf = new T[st];
                     cudaMemcpy(buf, decoder_output, sizeof(T) * st, cudaMemcpyDeviceToHost);
-                    const T step = input_tensors->at(4).getVal();
+                    T step = input_tensors->at(4).getVal();
                     if (step == 0) {
                         printf("decoder_output at layer %d step %d\n", l, step);
                         for (int i=0; i<50; i++) {
