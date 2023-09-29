861e5eec2429c39000d0aa2a20716ebd16735815
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-28 22:11:18 -0700

    commit

--- src/fastertransformer/kernels/unfused_attention_kernels.cu
+++ src/fastertransformer/kernels/unfused_attention_kernels.cu
@@ -1425,7 +1425,7 @@ __global__ void add_fusedQKV_bias_transpose_kernel(T*                           
     v = mmha::add(v, v_bias);
 
     if (!neox_rotary_style) {
-        mmha::apply_rotary_embedding(q, k, tidx, rotary_embedding_dim, dst_kv_seq_idx, rope_theta);
+        mmha::apply_rotary_embedding(q, k, tidx, rotary_embedding_dim, rope_theta, dst_kv_seq_idx);
     }
     else {
         const bool do_rotary = !is_masked && vec_size * tidx < rotary_embedding_dim;
@@ -1451,7 +1451,7 @@ __global__ void add_fusedQKV_bias_transpose_kernel(T*                           
             mmha::vec_from_smem_transpose(q, q_smem, transpose_idx, smem_pitch);
             mmha::vec_from_smem_transpose(k, k_smem, transpose_idx, smem_pitch);
 
-            mmha::apply_rotary_embedding(q, k, transpose_idx / tidx_factor, rotary_embedding_dim, dst_kv_seq_idx, rope_theta);
+            mmha::apply_rotary_embedding(q, k, transpose_idx / tidx_factor, rotary_embedding_dim, rope_theta, dst_kv_seq_idx);
 
             mmha::write_smem_transpose(q, q_smem, transpose_idx, smem_pitch);
             mmha::write_smem_transpose(k, k_smem, transpose_idx, smem_pitch);
