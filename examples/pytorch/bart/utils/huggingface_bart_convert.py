a469c03f7e21e0a12f72374c2742c1a0a74ee1e6
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-02 17:58:30 -0700

    commit

--- examples/pytorch/bart/utils/huggingface_bart_convert.py
+++ examples/pytorch/bart/utils/huggingface_bart_convert.py
@@ -275,10 +275,13 @@ def convert_checkpoint(args):
     np_weight_data_type = get_weight_data_type(args.weight_data_type)
 
     i_gpu_num = args.inference_tensor_para_size
-    pool = multiprocessing.Pool(args.processes)
-    pool.starmap_async(split_and_convert_process,
-                       [(name, param.cpu().detach().numpy().astype(np_weight_data_type), i_gpu_num, saved_dir)
-                        for name, param in model.state_dict().items()])
+
+    for name, param in model.state_dict().items():
+        split_and_convert_process(name, param.cpu().detach().numpy().astype(np_weight_data_type), i_gpu_num, saved_dir)
+    # pool = multiprocessing.Pool(args.processes)
+    # pool.starmap_async(split_and_convert_process,
+    #                    [(name, param.cpu().detach().numpy().astype(np_weight_data_type), i_gpu_num, saved_dir)
+    #                     for name, param in model.state_dict().items()])
 
     pool.close()
     pool.join()
