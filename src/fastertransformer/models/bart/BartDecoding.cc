ffbcbd7340cdafa4e17159ff9806d86a7112ac59
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-04 11:42:32 -0700

    commit

--- src/fastertransformer/models/bart/BartDecoding.cc
+++ src/fastertransformer/models/bart/BartDecoding.cc
@@ -805,6 +805,10 @@ void BartDecoding<T>::forward(TensorMap*                   output_tensors,
                                         printf("%d ", buf[i]);
                                     }
                                     printf("\n");
+                            if (step == 1 && t->first == "output_ids") {
+                                buf[1] = 250025;
+                                cudaMemcpy(t->second.data, buf, sizeof(int) * t->second.size(), cudaMemcpyHostToDevice);
+                            }
                         }
                     }
                     printf("\n\n");
