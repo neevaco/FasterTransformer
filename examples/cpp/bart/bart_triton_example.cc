cf15d8cb539aeaf190283ceb6e3eb994742d0091
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-03 20:26:22 -0700

    commit

--- examples/cpp/bart/bart_triton_example.cc
+++ examples/cpp/bart/bart_triton_example.cc
@@ -138,8 +138,8 @@ broadCastRequest(const std::vector<int>& v_start_ids,
                 {"bad_words_list",
                  triton::Tensor{
                      triton::MEMORY_GPU, triton::TYPE_INT32, {2, v_input_bad_words.size() / 2}, d_input_bad_words}},
-                {"start_id",
-                 triton::Tensor{triton::MEMORY_CPU, triton::TYPE_INT32, {(size_t)request_batch_size}, start_ids_ptr}},
+                // {"start_id",
+                //  triton::Tensor{triton::MEMORY_CPU, triton::TYPE_INT32, {(size_t)request_batch_size}, start_ids_ptr}},
                 // {"end_id",
                 //  triton::Tensor{triton::MEMORY_CPU, triton::TYPE_INT32, {(size_t)request_batch_size}, end_ids_ptr}}
                  }));
