7b31bd638544add45076c72be64d1b3f2c4c83ba
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-06 17:19:23 -0700

    commit

--- src/fastertransformer/kernels/llama/decoder_masked_groupedquery_attention/decoder_masked_groupedquery_attention_template.hpp
+++ src/fastertransformer/kernels/llama/decoder_masked_groupedquery_attention/decoder_masked_groupedquery_attention_template.hpp
@@ -1205,8 +1205,8 @@ __global__ void masked_groupedquery_attention_kernel(GroupedQuery_attention_para
     const int bkvhi = bi * params.num_kv_heads + kvhi;
     // Combine the "beam-aware" batch idx and the head indices.
     const int bbhi      = bbi * params.beam_width * params.num_heads + hi;
-    const int bbkvhi    = bbi * params.beam_width * params.num_kv_heads + kvhi;
-    printf("%ld\n", bbkvhi);
+    const size_t bbkvhi    = bbi * params.beam_width * params.num_kv_heads + kvhi;
+    printf("bbkvhi: %ld\n", bbkvhi);
     // The thread in the block.
     const int tidx = threadIdx.x;
 
