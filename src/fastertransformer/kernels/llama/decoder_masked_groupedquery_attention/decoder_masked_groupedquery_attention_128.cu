53bb8b30198b39d72b64ade8ae584d3e5f06e91a
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-07 18:57:42 -0700

    commit

--- src/fastertransformer/kernels/llama/decoder_masked_groupedquery_attention/decoder_masked_groupedquery_attention_128.cu
+++ src/fastertransformer/kernels/llama/decoder_masked_groupedquery_attention/decoder_masked_groupedquery_attention_128.cu
@@ -47,7 +47,7 @@ void mgqa_launch_kernel(const KERNEL_PARAMS_TYPE& params, const cudaStream_t& st
     int            tlength            = params.timestep;
     // printf("tlength, CROSS_ATTENTION = %d, %d\n", tlength);
     if (params.cache_indir == nullptr) {
-        if (tlength < 32000) {
+        if (tlength < 32) {
             printf("mgqa_launch_kernel, tlength: %d params.cache_indir: %d\n", tlength, params.cache_indir == nullptr);
             MGQA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 4, THREADS_PER_VALUE, 64, false, stream);
         }
