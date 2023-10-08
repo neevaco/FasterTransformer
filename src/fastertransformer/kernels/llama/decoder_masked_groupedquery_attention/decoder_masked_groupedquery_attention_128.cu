ffeeedb84a4d73f48e821418fb21fb22b821bf67
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-07 18:49:20 -0700

    commit

--- src/fastertransformer/kernels/llama/decoder_masked_groupedquery_attention/decoder_masked_groupedquery_attention_128.cu
+++ src/fastertransformer/kernels/llama/decoder_masked_groupedquery_attention/decoder_masked_groupedquery_attention_128.cu
@@ -29,7 +29,6 @@
     size_t smem_sz = mmha::smem_size_in_bytes<T>(params, THDS_PER_VALUE, THDS_PER_BLOCK);          \
     printf("smem_sz: %d\n", smem_sz);   \
     dim3   grid(params.num_heads, params.batch_size);                                                                  \
-    
     cudaFuncSetAttribute(mmha::masked_multihead_attention_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_sz);           \
     mmha::masked_groupedquery_attention_kernel<T,                                                                         \
                                             Dh,                                                                        \
