9d4a65045006ffca5ce92cb4640fa21c1d5bca67
Yahia Bsat <yahia.bsat@snowflake.com>
2023-11-16 23:34:13 -0800

    j

--- src/fastertransformer/layers/DynamicDecodeLayer.cc
+++ src/fastertransformer/layers/DynamicDecodeLayer.cc
@@ -448,7 +448,7 @@ void DynamicDecodeLayer<T>::forward(TensorMap* output_tensors, TensorMap* input_
         const size_t id_offset         = ite * local_batch_size * beam_width;
         const size_t stop_words_length = input_tensors->at("stop_words_list").shape[2];
 
-        printf('In stop words list');
+        printf("In stop words list");
 
         invokeStopWordsCriterion(output_tensors->at("output_ids").getPtr<const int>(),
                                  output_tensors->at("parent_ids").getPtr<const int>(),
